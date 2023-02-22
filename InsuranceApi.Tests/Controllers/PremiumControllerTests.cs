5c34c93160330a6493dca6495b99ca2bbe8a6689
Abinash <abinash.mdr@gmail.com>
2023-02-22 11:14:02 +1100

    Add first test for PremiunController

---
+++ InsuranceApi.Tests/Controllers/PremiumControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using InsuranceApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceApi.Tests.Controllers;
+
+public class PremiumControllerTests
+{
+    [Fact]
+    public async Task Get_OnSuccess_Returns200StatusCode()
+    {
+        PremiumController controller = new();
+
+        var result = (ObjectResult) await controller.CalculatePremium();
+
+        result.StatusCode.Should().Be(200);
+    }
+}
